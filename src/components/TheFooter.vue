bae48b7744dd7ec6cd655eff997e70e578187f2b
alessandrostorchi92 <storchi.alle@gmail.com>
2023-08-24 09:30:20 +0200

    footer ok

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -61,7 +61,6 @@ export default {
 </script>
 
 <template>
-
     <footer>
 
         <!-- Sezione footer top   -->
@@ -82,7 +81,7 @@ export default {
                                     <a class="nav-link" aria-current="page" href="#">{{ link.name }}</a>
                                 </li>
                             </ul>
-            
+
                             <h5 class="title mt-2">SHOP</h5>
                             <ul class="nav flex-column">
                                 <li class="nav-item" v-for="link in ShopLinks">
@@ -90,7 +89,7 @@ export default {
                                 </li>
                             </ul>
                         </div>
-            
+
                         <!-- Sezione DC  -->
                         <div>
                             <h5 class="title">DC</h5>
@@ -100,7 +99,7 @@ export default {
                                 </li>
                             </ul>
                         </div>
-            
+
                         <!-- Sezione Sites  -->
                         <div>
                             <h5 class="title">SITES</h5>
@@ -115,8 +114,13 @@ export default {
 
                     <div class="col-6">
 
-                        <!-- Immagine Logo  -->
-                        <img src="../assets/img/dc-logo-bg.png" alt="Immagine Logo">
+                        <div class="logo-container">
+
+                            <!-- Immagine Logo  -->
+                            <img src="../assets/img/dc-logo-bg.png" alt="Immagine Logo">
+
+                        </div>
+
 
                     </div>
 
@@ -128,46 +132,47 @@ export default {
 
         <!-- Sezione footer bottom  -->
 
-        <div class="container">
+        <section class="section-footer-bottom">
 
-            <div class="d-flex justify-content-around justify-content-center align-items-center">
+            <div class="container h-100">
 
-                <!-- Sezione di sinistra  -->
+                <div class="h-100 d-flex justify-content-center align-items-center justify-content-between">
 
-                <!-- Call to Action Sign Up -->
-                <button type="button" class="btn border-primary border-2 p-2 fw-semibold fs-5">SIGN-UP NOW!</button>
-
-                <!-- Sezione di destra  -->
-
-                <div>
-
-                    <!-- Call to Action Follow Us -->
-                    <span class="fw-semibold fs-4">FOLLOW US</span>
-
-                    <!-- Social Icons  -->
-
-                    <!-- // Questa procedimento mi permette di stampare le immagini dinamicamente su vite-vue  -->
-                    <div v-for="icon in SocialIcons">
-                        <img :src="getImagePath(`../assets/img/${icon.img}`)" alt="">
+                    <!-- Left Section  -->
+    
+                    <button type="button" class="btn border-primary border-2 p-2 fw-bold fs-5 text-white">SIGN-UP NOW</button>
+    
+                    <!-- Right Section  -->
+    
+                    <div class="d-flex align-items-center gap-2">
+    
+                        <span class="fw-bold fs-5 text-white">FOLLOW US</span>
+    
+                        <!-- // Questa procedimento mi permette di stampare le immagini dinamicamente su vite-vue  -->
+                        <div v-for="icon in SocialIcons">
+                            <img :src="getImagePath(`../assets/img/${icon.img}`)" alt="">
+                        </div>
+    
                     </div>
 
                 </div>
 
             </div>
 
-        </div>
+        </section>
+
 
     </footer>
+
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/_variables.scss" as *;
-
+@use "../styles/partials/mixins";
 
 .section-footer-top {
     background: url("../assets/img/footer-bg.jpg");
-    background-size: cover;
-    height: 500px;
+    height: 480px;
     overflow: hidden;
 }
 
@@ -181,6 +186,12 @@ a {
     color: #959595;
     white-space: nowrap;
     font-size: 0.8rem;
-    padding: 0.2rem;  
+    padding: 0.2rem;
 }
+
+.section-footer-bottom {
+    background-color: #303030;
+    height: 100px;
+}
+
 </style>
